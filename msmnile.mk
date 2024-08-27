879c8bd798b614d73079d7c77c4cf30324437418
hirero-kazuwa <hirero00@gmail.com>
2024-08-27 15:49:11 +0000

    sm8150-common: build vendor.qti.hardware.memtrack-service

--- msmnile.mk
+++ msmnile.mk
@@ -198,8 +198,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
     gralloc.msmnile \
     hwcomposer.msmnile \
     libdisplayconfig.qti \
@@ -208,7 +206,6 @@ PRODUCT_PACKAGES += \
     libsdmutils \
     libtinyxml \
     libvulkan \
-    memtrack.msmnile \
     vendor.display.config@1.5 \
     vendor.display.config@1.11.vendor \
     vendor.display.config@2.0 \
@@ -217,7 +214,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@1.1.vendor \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
-    vendor.qti.hardware.display.mapper@4.0.vendor
+    vendor.qti.hardware.display.mapper@4.0.vendor \
+    vendor.qti.hardware.memtrack-service
 
 # Dolby
 PRODUCT_COPY_FILES += \
@@ -235,10 +233,9 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor
 
 # fastbootd
-ifeq ($(TARGET_USE_DYNAMIC_PARTITIONS),true)
 PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.1-impl.custom \
     fastbootd
-endif
 
 # Fingerprint
 PRODUCT_PACKAGES += \
